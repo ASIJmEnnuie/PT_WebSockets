a941f711df6e281e33c00beb3387e44d9d4de482
MacBootglass <thibault.theologien@insa-rouen.fr>
2016-10-16 17:42:56 +0200

    Ajout d'une connexion à un serveur java de test des websockets stomp (localhost:8080)

--- client/src/ressources/app/PT_Global.js
+++ client/src/ressources/app/PT_Global.js
@@ -1,20 +1,56 @@
 import React from 'react';
 import PT_EventList from './PT_EventList';
 import PT_ControlBar from './PT_ControlBar';
+import SockJS from 'sockjs-client';
+import Stomp from 'stompjs';
 
 var donnees = require("../datas/events.json").events;
 
 const PT_Global = React.createClass({
+  componentDidMount: function() {
+    this.setState(function(previousState, currentProps) {
+      var stompClient = Stomp.over(new SockJS('http://localhost:8080/gs-guide-websocket'));
+
+      stompClient.connect({}, function (frame) {
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/greetings', function (greeting) {
+          console.log(JSON.parse(greeting.body).content);
+        });
+      });
+
+      return {
+        stompClient: stompClient
+      };
+    });
+  },
+
+  getInitialState: function() {
+    return {
+      stompClient: null
+    }
+  },
+
   dateController: function(e, date) {
     console.log("Recherche de la date suivante: " + date);
   },
 
-  placeController: function(e, x, y, z) {
+  placeController: function(e) {
     console.log("Recherche de l'emplacement: " + e.target.value);
   },
 
   razController: function() {
     console.log("Remise à zéro des recherches");
+  },
+
+  disconnect: function() {
+    if (this.state.stompClient != null) {
+      this.statet.stompClient.disconnect();
+    }
+    console.log("Disconnected");
+  },
+
+  sendName: function() {
+    this.state.stompClient.send("/app/hello", {}, JSON.stringify({'name': 'Thibault'}));
   },
 
   render: function() {
