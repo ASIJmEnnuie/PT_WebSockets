d68b1b73f77773178ad1d243851aa5f7fe04299b
MacBootglass <thibault.theologien@insa-rouen.fr>
2016-10-20 22:03:27 +0200

    correction serveur (allow origins) + connexion au client

--- client/src/ressources/app/PT_Global.js
+++ client/src/ressources/app/PT_Global.js
@@ -4,53 +4,78 @@ import PT_ControlBar from './PT_ControlBar';
 import SockJS from 'sockjs-client';
 import Stomp from 'stompjs';
 
-var donnees = require("../datas/events.json").events;
+function zerosBeforeNumbers(n) {
+  if (n < 10){
+    return "0"+n;
+  }
+  else {
+    return n;
+  }
+}
 
 const PT_Global = React.createClass({
   componentDidMount: function() {
     this.setState(function(previousState, currentProps) {
       var stompClient = Stomp.over(new SockJS('http://localhost:8080/gs-guide-websocket'));
+      var composant = this;
 
       stompClient.connect({}, function (frame) {
-        console.log('Connected: ' + frame);
-        stompClient.subscribe('/topic/greetings', function (greeting) {
-          console.log(JSON.parse(greeting.body).content);
+        stompClient.subscribe('/topic/listeEvenementsDate', function (getEventsByDate) {
+          composant.setState({donnees:JSON.parse(getEventsByDate.body)});
+        });
+
+        stompClient.subscribe('/topic/listeEvenementsLieu', function (getEventsByLieu) {
+          composant.setState({donnees:JSON.parse(getEventsByLieu.body)});
+        });
+
+        stompClient.subscribe('/topic/listeEvenements', function (getAllEvents) {
+          console.log("____________HERE___________");
+          composant.setState({donnees:JSON.parse(getAllEvents.body)});
         });
       });
 
       return {
-        stompClient: stompClient
+        stompClient: stompClient,
+        donnees: []
       };
     });
   },
 
   getInitialState: function() {
     return {
-      stompClient: null
+      stompClient: null,
+      donnees: []
     }
   },
 
-  dateController: function(e, date) {
-    console.log("Recherche de la date suivante: " + date);
+  dateController: function(e, dateRecept) {
+    console.log("Recherche de la date suivante: " + dateRecept);
+    var date = new Date(dateRecept);
+    var day = zerosBeforeNumbers(date.getDate());
+    var month = zerosBeforeNumbers(date.getMonth()+1);
+    var year = date.getFullYear();
+    var dateSend = day+"/"+month+"/"+year;
+
+    this.state.stompClient.send("/app/evenementsDate", {}, JSON.stringify({'message': dateSend}));
   },
 
   placeController: function(e) {
-    console.log("Recherche de l'emplacement: " + e.target.value);
+    if (e.target.value.length <= 0) {
+      this.razController();
+    }
+    else {
+      this.state.stompClient.send("/app/evenementsLieu", {}, JSON.stringify({'message': e.target.value}));
+    }
   },
 
   razController: function() {
-    console.log("Remise à zéro des recherches");
+    this.state.stompClient.send("/app/evenements", {}, "");
   },
 
   disconnect: function() {
     if (this.state.stompClient != null) {
-      this.statet.stompClient.disconnect();
+      this.state.stompClient.disconnect();
     }
-    console.log("Disconnected");
-  },
-
-  sendName: function() {
-    this.state.stompClient.send("/app/hello", {}, JSON.stringify({'name': 'Thibault'}));
   },
 
   render: function() {
@@ -62,7 +87,7 @@ const PT_Global = React.createClass({
           razController={this.razController}
         />
         <PT_EventList
-          events={donnees}
+          events={this.state.donnees}
         />
       </div>
     );
